759c6036ba0cced22eabbccd37577eed085fad03
hibataha <h.kasem@whitestork.me>
2022-12-13 10:36:42 +0200

    remove articles

--- articles.js
+++ articles.js
@@ -1,17 +1,5 @@
 
 module.exports = {
-  posts: [
-    {
-      "title": "POC: FETCH BASE ",
-      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
-      "id": 1
-    },
-    {
-      "title": "POC: AXIOS",
-      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
-      "id": 2
-    }
-  ],
   articles: [
     {
       "title": "POC: AXIOS  ",
